a8974629e766769305d2226967d509e4c54f071d
Marek Kowalkiewicz <marek@kowalkiewicz.net>
2013-12-22 13:45:14 +0800

    Initial commit

---
+++ ex1.py
@@ -0,0 +1,8 @@
+__author__ = 'marek'
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print "Yay! Printing."
+print "I'd rather you 'not'."
+print 'I "said" do not touch this.'
